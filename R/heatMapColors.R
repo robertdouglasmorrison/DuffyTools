1ac8861eca20edc2016e9a61ba9d036a51b121b3
Robert <robert.morrison@nih.gov>
2020-03-23 13:24:32 -0700

    adding support for Cell Type heatmaps

--- R/heatMapColors.R
+++ R/heatMapColors.R
@@ -1,21 +1,36 @@
 
 # heatMapColors.R - make a green to red color ramp
 
-heatMapColors <- function( nColors, inflexionPoint=0.5, plotRamp=TRUE) {
+heatMapColors <- function( nColors, palette=c("red-black-green", "red-white-blue"),
+				inflexionPoint=0.5, rampExponent=0.5, plotRamp=TRUE, pt.cex=30/sqrt(nColors)) {
 
-	# make a color map...  we are dividing R & G by transcriptome...
-	redFraction <- inflexionPoint
-	nbig <- round( nColors * redFraction)
-	nsmall <- nColors - nbig
-	zsmall <- rep( 0, times=nsmall)
-	zbig <- rep( 0, times=nbig)
-	rampup <- seq( 0.02,1,length.out=nbig)^0.5
-	rampdown <- seq( 1,0.02,length.out=nsmall)^0.5
-	colmap <- rgb( red=c( zsmall, rampup), green=c( rampdown, zbig), blue=0)
-	colHgt <- c( -rampdown, rampup)
+	# make a color map...  for use by heatmaps, etc.
+	palette <- match.arg( palette)
+
+	if ( nColors < 3) nColors <- 5
+	upFraction <- inflexionPoint
+	nUp <- round( nColors * upFraction)
+	nDown <- nColors - nUp
+	smallStep <- max( round( 1/nColors, digits=2), 0.001)
+	rampUp <- seq( smallStep, 1, length.out=nUp) ^ rampExponent
+	rampUp[ nUp] <- 1.0
+	rampDown <- seq( 1, smallStep, length.out=nDown) ^ rampExponent
+	rampDown[ nDown] <- smallStep
+	zeroDown <- rep.int( 0, nDown)
+	zeroUp <- rep.int( 0, nUp)
+	oneDown <- rep.int( 1, nDown)
+	oneUp <- rep.int( 1, nUp)
+
+	# different methods, based on the palette
+	if ( palette == "red-black-green") {
+		colmap <- rgb( red=c( zeroDown, rampUp), green=c( rampDown, zeroUp), blue=rep.int(0,nColors))
+	} else if (palette == "red-white-blue") {
+		colmap <- rgb( red=c( rev(rampDown), oneUp), green=c( rev(rampDown), rev(rampUp)), blue=c( oneDown, rev(rampUp)))
+	}
+	colHgt <- c( -rampDown, rampUp)
 
 	# do the heatmap...
-	if (plotRamp) plot( 1:nColors, colHgt, pch=21, cex=8, bg=colmap)
+	if (plotRamp) plot( 1:nColors, colHgt, pch=21, cex=pt.cex, bg=colmap)
 
 	return( colmap)
 }
