6da5f6736d61e77eb3885169c5ddd78e520c4959
Robert Morrison <robert.morrison@nih.gov>
2025-04-29 16:15:36 -0700

    added extra context info about multiple low value stairstep data points

--- R/ROC_Tools.R
+++ R/ROC_Tools.R
@@ -40,7 +40,7 @@ duffy.ROC <- function( goodScores, badScores, label="ROC curve: ", visualize=TRU
 
 	if (visualize) {
 		#par( mai=c(1,1,0.8,0.4))
-		plot( mspec, msens, main=label, xlim=c(0,1.1),
+		plot( mspec, msens, main=label, xlim=c(0,1.1), ylim=c(0,1.04),
 			xlab="1 - Specificity", ylab="Sensitivity", type="p", font.axis=2, font.lab=2)
 
 		# try to show some values in a sensible way
@@ -55,6 +55,18 @@ duffy.ROC <- function( goodScores, badScores, label="ROC curve: ", visualize=TRU
 				digits=ndigShow), pos=posOffset[1], srt=rotateAngle[1], offset=1.0, cex=label.cex)
 		text( mspec[show[isLess]], msens[show[isLess]], formatC( cuts[show[isLess]], format="f", 
 				digits=ndigShow), pos=posOffset[2], srt=rotateAngle[2], offset=1.5, cex=label.cex)
+
+		# Patrick had an ask, to know how many observations land at a single location on the ROC curve.
+		# for now focus on the upper right corner where the smallest "bad" value is represented.
+		# But how we ask depends on which direction "bad" went..
+		if ( mean(goodScores) > mean(badScores)) {
+			nMostBad <- sum( badScores == min(badScores)) 
+		} else {
+			nMostBad <- sum( goodScores == min(goodScores)) 
+		}
+		pctChange <- ((msens[1]-msens[2]) / max(mspec[1]-mspec[2], 0.1)) 
+		mySRT <- round( pctChange * 45)
+		if ( nMostBad > 4) text( mean(mspec[1:2]), mean(msens[1:2]), paste( "Nobs = ",nMostBad,sep=""), pos=3, srt=mySRT, offset=0.75, cex=label.cex*0.75)
 
 		# show the curve and the diagonal
 		lines( c(0,1), c(0,1), col=1, lty=3)
