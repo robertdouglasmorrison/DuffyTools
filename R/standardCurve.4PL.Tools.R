9cb65c22717576958b69f983bb6090e80ec4e47a
Robert Morrison <robert.morrison@nih.gov>
2025-11-26 07:36:48 -0800

    adding linear versions (2PL) to supplement the 4PL functions

--- R/standardCurve.4PL.Tools.R
+++ R/standardCurve.4PL.Tools.R
@@ -11,6 +11,9 @@
 # C = inflexion point (midpoint)
 # D = highest asymptote (high)
 
+# as of 2025, adding a 'linear' version, to accommodate assays that need linear fitting.
+# Calling this new linear method '2PL' curves
+
 
 `forward.4pl` <- function(x, A, B, C, D) { return( D + ((A - D) / (1 + (x / C) ^ B)))}
 
@@ -18,7 +21,8 @@
 
 
 `calculateStandardCurveABCD` <- function( odValues, concValues, plot=FALSE, label="", las=1, digits=4, 
-					pt.col=4, lin.col=1, fit.col=4, add.plot=FALSE, show.legend=!add.plot) {
+					pt.col=4, lin.col=1, fit.col=4, add.plot=FALSE, 
+					show.legend=!add.plot, ...) {
 
 	require( minpack.lm)
 
@@ -30,7 +34,7 @@
 	concValues <- concValues[useV]
 	NCV <- length(concValues)
 	if ( length(odValues)!= NCV || NCV < 5) {
-		cat( "\nError: not enough OD & Concentration values to fit: ", NCV)
+		cat( "\nError: not enough OD & Concentration values to fit 4PL: ", NCV)
 		return(NULL)
 	}
 	
@@ -79,7 +83,7 @@
 		# either create a new plot, or just add data to existing
 		if ( ! add.plot) {
 			plot.default( xPtr, odValues, type="p", pch=1, col=1, xlab="Concentration", ylab="OD Values", 
-					xaxt="n", main=paste("Standard Curve Fit: ", label))
+					xaxt="n", main=paste("4PL Standard Curve Fit: ", label), ...)
 			axis( side=1, at=1:NCONC, round( as.numeric(levels(concFac)), digits=digits), las=las)
 		}
 		lines( 1:NCONC, avgODobs, lty=1, col=lin.col)
@@ -93,4 +97,78 @@
 	}
 
 	return( out)
+}
+
+
+# 2PL version, use linear regression to implement the Y = mX + b methods, but still plot them on log-linear scalings
+
+`forward.2pl` <- function(x, M, B) { return( B + (M*x))}
+
+`inverse.2pl` <- function( y, M, B) { return( (y-B) / M)}
+
+
+`calculateStandardCurveMB.2PL` <- function( odValues, concValues, plot=FALSE, label="", las=1, digits=4, 
+					pt.col=4, lin.col=1, fit.col=4, add.plot=FALSE, 
+					show.legend=!add.plot, ...) {
+
+	# prep the OD and Concentration values
+	odValues <- as.numeric( odValues)
+	concValues <- as.numeric( concValues)
+	useV <- which( ! is.na( odValues))
+	odValues <- odValues[useV]
+	concValues <- concValues[useV]
+	NCV <- length(concValues)
+	if ( length(odValues)!= NCV || NCV < 3) {
+		cat( "\nError: not enough OD & Concentration values to fit 2PL: ", NCV)
+		return(NULL)
+	}
+	
+	# we can now do the 2PL fit to deduce the best M/B (slope & Y-intercept) variables.  
+	# M = slope of straight line
+	# B = Y intercept at X=0 axis
+	
+	lmAns <- lm( odValues ~ concValues)
+	lmAns2 <- coef( summary( lmAns))
+	
+	out1 <- round( lmAns2[ , "Estimate"], digits=6)
+	names(out1) <- c( "B", "M")
+	
+	# also use this model to predict the OD values we should have seen for our inputs
+	odPredicted <- predict( lmAns)
+	r2 <- cor( odValues, odPredicted) ^ 2
+	
+	out2 <- c( out1, "R^2"=round( r2, digits=6))	
+	out <- list( "estimate"=out2, model=lmAns)
+	
+	if ( plot) {
+		# make the plot have the expected direction, with high OD at the right
+		ord <- order( concValues, odValues, decreasing=FALSE)
+		concValues <- concValues[ord]
+		odValues <- odValues[ord]
+		odPredicted <- odPredicted[ord]
+		concFac <- factor( concValues)
+		NCONC <- nlevels(concFac)
+		xPtr <- tapply( 1:NCV, concFac, FUN=NULL)
+		avgODobs <- tapply( odValues, concFac, mean)
+		avgODpred <- tapply( odPredicted, concFac, mean)
+
+		# we may have replicates of the concentrations.  So deduce how many unique values
+
+		# either create a new plot, or just add data to existing
+		if ( ! add.plot) {
+			plot.default( xPtr, odValues, type="p", pch=1, col=1, xlab="Concentration", ylab="OD Values", 
+					xaxt="n", main=paste("2PL (Linear) Standard Curve Fit: ", label), ...)
+			axis( side=1, at=1:NCONC, round( as.numeric(levels(concFac)), digits=digits), las=las)
+		}
+		lines( 1:NCONC, avgODobs, lty=1, col=lin.col)
+		points( xPtr, odPredicted, pch=2, col=pt.col)
+		lines( 1:NCONC, avgODpred, lty=2, col=fit.col)
+		if ( show.legend) {
+			legend( 'bottomright', c("Observed","Fitted"), pch=c(1,2), col=c(1,4), lwd=2)
+			outNames <- names(out2)
+			legend( 'topleft', paste( outNames, "=", round(out2, digits=digits)))
+		}
+	}
+
+	return( out2)
 }
